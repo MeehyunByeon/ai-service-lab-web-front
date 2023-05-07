ede71fdea9a6b7074fdd906f720044580c6c7e75
simwoongseob <simwoongseob@gmail.com>
2023-05-07 18:17:49 +0900

    ws_first-commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>웅섭이가 수정함</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
